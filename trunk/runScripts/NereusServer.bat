a978725fa39192745919288cfd5ccff2ea238aee
walzsl <walzsl@6020b621-f9f1-0310-b8bb-c2826e6a5e02>
2005-04-13 13:21:22 +0000

    Änderung der Startskriptnamen von MASIM* in Nereus*

---
+++ trunk/runScripts/NereusServer.bat
@@ -0,0 +1,1 @@
+java -Djava.rmi.codebase=http:///localhost:2002/ -Djava.rmi.server.hostname=localhost -Djava.security.policy=serverLocal.policy simulator.MALServer localhost F:\Systeme\FGML\server\scenario\
---
+++ trunk/runScripts/NereusServer.sh
@@ -0,0 +1,1 @@
+java -Djava.rmi.server.hostname=localhost -Djava.security.policy=../config/server.policy -cp  ../libs/shared.jar:../libs/weka.jar:../source/server simulator.MASIMServer localhost ../source/server
