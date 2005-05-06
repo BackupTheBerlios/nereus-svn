e729a8b0dc978b8e173d05f2ad55bce597713376
mango3 <mango3@6020b621-f9f1-0310-b8bb-c2826e6a5e02>
2005-05-06 10:32:36 +0000

    nomsg

--- branches/philip/runScripts/MALServer.bat
+++
@@ -1,1 +0,0 @@
-java -Djava.rmi.server.hostname=localhost -Djava.security.policy=serverLocal.policy -cp E:\CD\runtime\server\;E:\CD\runtime\server\lib\weka.jar;E:\CD\runtime\server\lib\shared.jar simulator.MASIMServer localhost E:\CD\runtime\server\scenario\
--- branches/philip/runScripts/MASIMServer.sh
+++
@@ -1,1 +0,0 @@
-java -Djava.rmi.server.hostname=localhost -Djava.security.policy=../config/server.policy -cp  ../libs/shared.jar:../libs/weka.jar:../source/server simulator.MASIMServer localhost ../source/server
