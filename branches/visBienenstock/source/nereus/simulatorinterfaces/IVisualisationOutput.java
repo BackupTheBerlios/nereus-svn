d481ad2ace79904c02095663758d90de9327dd4c
lippold <lippold@6020b621-f9f1-0310-b8bb-c2826e6a5e02>
2005-06-10 17:34:36 +0000

    Interface für die Ausgabe der zu visualisierenden Daten.

---
+++ branches/visBienenstock/source/nereus/simulatorinterfaces/IVisualisationOutput.java
@@ -0,0 +1,43 @@
+/*
+ * Dateiname      : IVisualisationOutput.java
+ * Erzeugt        : 26. Mai 2005
+ * Letzte Änderung: 10. Juni 2005 durch Dietmar Lippold
+ * Autoren        : Samuel Walz (samuel@gmx.info)
+ *
+ * Diese Datei gehört zum Projekt Nereus (http://nereus.berlios.de/).
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package nereus.simulatorinterfaces;
+
+/**
+ * Definiert die Schnittstelle zur Ausgabe der Date der Visualisierung auf
+ * Client-Seite.
+ *
+ * @author  Dietmar Lippold
+ */
+public interface IVisualisationOutput {
+
+    /**
+     * Übergibt einen neuen Datensatz zur Visualisierung.
+     *
+     * @param visObject  Der auszugebende Datensatz.
+     */
+    public void visualisiere(Object visObject);
+}
+
