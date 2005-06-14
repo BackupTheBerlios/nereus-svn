7c232fe03ffac471f188942839761acad5345ead
lippold <lippold@6020b621-f9f1-0310-b8bb-c2826e6a5e02>
2005-06-14 09:27:47 +0000

    Die für die Visualisierung in der Anmelde-Gui erforderichen Klassen als jar-Datei erstellt und in den run-Scripten aufgenommen.

--- branches/visBienenstock/runScripts/NereusClient.bat
+++ branches/visBienenstock/runScripts/NereusClient.bat
@@ -1,11 +1,15 @@
 call setup.bat
 
-set MAIN=nereus.registrationgui.MASIMClient
+REM Datei in der die Rechte für den Client vergeben werden.
 set POLICY_CONF=client.policy
 
 REM die Parameter für die JVM
-
 set JVMPAR= -Djava.security.policy=%CONFIG_DIR%\%POLICY_CONF%
 set JVMPAR=%JVMPAR% -Djava.rmi.server.hostname=%SERVER_HOST%
 
-%JAVA% %JVMPAR% -cp %DIST_DIR%\Client.jar;%SCENARIOS_DIR%\Scenarios.jar %MAIN% %SERVER_HOST% %BASIS_PFAD% %CONFIGFILE_CLIENT%
+REM die Main-Klasse ist
+REM nereus.client.MASIMClient
+REM und wird als Main-Class Attribut im jar-Manifest definiert
+set MAIN=nereus.registrationgui.MASIMClient
+
+%JAVA% %JVMPAR% -cp %DIST_DIR%\Client.jar;%DIST_DIR%\BienenstockVis.jar %MAIN% %SERVER_HOST% %BASIS_PFAD% %CONFIGFILE_CLIENT%
--- branches/visBienenstock/runScripts/NereusClient.sh
+++ branches/visBienenstock/runScripts/NereusClient.sh
@@ -2,14 +2,14 @@
 
 # Startet den Client
 
-# Ben?tigte Umgebungsvariablen (CONFIG_DIR, JAVA, DIST_DIR, BASIS_PFAD, SERVER_HOST)
-# werden gesetzt
+# Benötigte Umgebungsvariablen (CONFIG_DIR, JAVA, DIST_DIR, BASIS_PFAD,
+# SERVER_HOST) werden gesetzt
 . ./setup.sh
 
-# Datei in der die Rechte fuer den Client vergeben werden.
+# Datei in der die Rechte für den Client vergeben werden.
 POLICY_CONF=client.policy
 
-# die Parameter f?r die JVM
+# die Parameter für die JVM
 JVMPAR=" " 
 JVMPAR="$JVMPAR -Djava.security.policy=$CONFIG_DIR/$POLICY_CONF"
 JVMPAR="$JVMPAR -Djava.rmi.server.hostname=$SERVER_HOST"
@@ -17,6 +17,7 @@ JVMPAR="$JVMPAR -Djava.rmi.server.hostname=$SERVER_HOST"
 # die Main-Klasse ist
 # nereus.client.MASIMClient
 # und wird als Main-Class Attribut im jar-Manifest definiert
-
 MAIN="nereus.registrationgui.MASIMClient"
-exec $JAVA $JVMPAR -cp $DIST_DIR/Client.jar:$SCENARIOS_DIR/Scenarios.jar $MAIN $SERVER_HOST $BASIS_PFAD $CONFIGFILE_CLIENT
+
+exec $JAVA $JVMPAR -cp $DIST_DIR/Client.jar:$DIST_DIR/BienenstockVis.jar $MAIN $SERVER_HOST $BASIS_PFAD $CONFIGFILE_CLIENT
+
