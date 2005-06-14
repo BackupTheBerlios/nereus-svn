3397c1419e4f3df64749c2e5424c0cc878aa3bff
lippold <lippold@6020b621-f9f1-0310-b8bb-c2826e6a5e02>
2005-06-14 11:56:02 +0000

    Scenario.jar umbenannt in BienenstockScenario.jar, aus der Distributions-Datei vom Client zusammen mit dem Verzeichnis scenarios entfernt und beim Server aus scenarios nach dist verschoben.

--- branches/visBienenstock/runScripts/NereusServer.sh
+++ branches/visBienenstock/runScripts/NereusServer.sh
@@ -2,15 +2,14 @@
 
 # Startet den Server
 
-# Benoetigte Umgebungsvariablen (CONFIG_DIR, JAVA,SERVER_HOST,
-# BASIS_PFAD, DIST_DIR)
-# werden gesetzt
+# Benötigte Umgebungsvariablen (CONFIG_DIR, JAVA,SERVER_HOST, BASIS_PFAD,
+# DIST_DIR) werden gesetzt
 . ./setup.sh
 
-# Datei in der die Rechte fuer den Server vergeben werden.
+# Datei in der die Rechte für den Server vergeben werden.
 POLICY_CONF=server.policy
 
-# die Parameter fr die JVM
+# die Parameter für die JVM
 JVMPAR=" " 
 JVMPAR="$JVMPAR -Djava.security.policy=$CONFIG_DIR/$POLICY_CONF"
 JVMPAR="$JVMPAR -Djava.rmi.server.hostname=$SERVER_HOST"
@@ -19,5 +18,6 @@ JVMPAR="$JVMPAR -Djava.rmi.server.hostname=$SERVER_HOST"
 # nereus.simulator.MASIMServer
 # und wird als Main-Class Attribut im jar-Manifest definiert
 MAIN="nereus.simulator.MASIMServer"
-exec $JAVA $JVMPAR -cp $DIST_DIR/Server.jar:$SCENARIOS_DIR/Scenarios.jar $MAIN $SERVER_HOST $BASIS_PFAD $CONFIGFILE_SERVER
+
+exec $JAVA $JVMPAR -cp $DIST_DIR/Server.jar:$DIST_DIR/BienenstockScenario.jar $MAIN $SERVER_HOST $BASIS_PFAD $CONFIGFILE_SERVER
 
