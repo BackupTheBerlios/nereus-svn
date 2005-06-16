9e002babfb51040414376b5ae043bca9ce7b422d
lippold <lippold@6020b621-f9f1-0310-b8bb-c2826e6a5e02>
2005-06-16 08:33:08 +0000

    Löschen eines veralteten Entwicklungszweigs.

--- branches/szenario_visualisation/runScripts/NereusClient.sh
+++
@@ -1,1 +0,0 @@
-java -Djava.rmi.server.hostname=localhost -Djava.security.policy=../config/client.policy -cp ../libs/weka.jar:../libs/shared.jar:../source/client/ client.MASIMClient localhost ../source/client
--- branches/szenario_visualisation/runScripts/NereusClientWebserver.sh
+++
@@ -1,1 +0,0 @@
-java -classpath ../source/client/ webserver.ClassFileServer 2003 ../source/client/
